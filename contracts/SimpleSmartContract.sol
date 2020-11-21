8ce80c07bec2ea6581ad2d808d7660ec985e3e62
jackdisalvatore <jackp.disalvatore@gmail.com>
2020-11-21 11:16:59 -0600

    adding string storage

--- contracts/SimpleSmartContract.sol
+++ contracts/SimpleSmartContract.sol
@@ -2,6 +2,7 @@ pragma solidity >=0.4.22 <0.8.0;
 
 contract SimpleSmartContract {
     string public data;
+    uint[] public ids;
 
     // Transaction (changes state)
     function set(string memory _data) public {
@@ -11,5 +12,27 @@ contract SimpleSmartContract {
     // Call (reads state)
     function get() view public returns (string memory) {
         return data;
+    }
+
+    // Transaction to add ID to array
+    function addId(uint id) public {
+        ids.push(id);
+    }
+
+    // Call to read ID by position
+    // This is reduntant becuase all member variables come
+    // with built in getter functions
+    function getId(uint index) view public returns (uint) {
+        return ids[index];
+    }
+
+    // Call to read all IDs
+    function getIds() view public returns (uint[] memory) {
+        return ids;
+    }
+
+    // Call to read ID array length
+    function getIdLength() view public returns (uint) {
+        return ids.length;
     }
 }
--- test/simpleSmartContract.js
+++ test/simpleSmartContract.js
@@ -1,15 +1,39 @@
 const SimpleSmartContract = artifacts.require('SimpleSmartContract')
 
 contract('SimpleSmartContract', async () => {
+  let simpleSmartContract = null
 
-  it('Should properly delpoy the smart contract', async () => {
-    const simpleSmartContract = await SimpleSmartContract.deployed()
+  before('Should properly delpoy the smart contract', async () => {
+    simpleSmartContract = await SimpleSmartContract.deployed()
     console.log('\tAddress is: ' +  simpleSmartContract.address)
     assert(simpleSmartContract.address !== '')
 
     await simpleSmartContract.set('Hello World')
     const data = await simpleSmartContract.get()
     assert(data === 'Hello World')
+  })
+
+  it('Should add an ID to the ids array', async () => {
+    await simpleSmartContract.addId(10)
+    // Use default getter
+    const result = await simpleSmartContract.ids(0)
+    assert(result.toNumber() === 10)
+  })
+
+  it('Should read an ID from the ids array', async () => {
+    const result = await simpleSmartContract.getId(0)
+    assert(result.toNumber() === 10)
+  })
+
+  it('Should read all IDs from the ids array', async () => {
+    const result = await simpleSmartContract.getIds()
+    const ids = result.map(id => id.toNumber())
+    assert.deepEqual(ids, [10])
+  })
+
+  it('Should read ID array length', async () => {
+    const result = await simpleSmartContract.getIdLength()
+    assert(result.toNumber() === 1)
   })
 
 })
